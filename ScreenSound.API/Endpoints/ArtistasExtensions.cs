e2c13242d2e5e3c15a6be6d705f007950e46dc1f
fabinho1992 <f.santosdev1992@gmail.com>
2024-05-22 09:59:54 -0300

    Iniciando repositório

---
+++ ScreenSound.API/Endpoints/ArtistasExtensions.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScreenSound.API.Requests;
+using ScreenSound.API.Response;
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.API.Endpoints;
+
+public static class ArtistasExtensions
+{
+    public static void AddEndPointsArtistas(this WebApplication app)
+    {
+        var group = app.MapGroup("artistas")
+            .RequireAuthorization()
+            .WithTags("Artistas");
+
+        #region Endpoint Artistas
+        group.MapGet("", ([FromServices] DAL<Artista> dal) =>
+        {
+            var listaDeArtistas = dal.Listar();
+            if (listaDeArtistas is null)
+            {
+                return Results.NotFound();
+            }
+            var listaDeArtistaResponse = EntityListToResponseList(listaDeArtistas);
+            return Results.Ok(listaDeArtistaResponse);
+        });
+
+        group.MapGet("{nome}", ([FromServices] DAL<Artista> dal, string nome) =>
+        {
+            var artista = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+            if (artista is null)
+            {
+                return Results.NotFound();
+            }
+            return Results.Ok(EntityToResponse(artista));
+
+        });
+
+        group.MapPost("", async ([FromServices]IHostEnvironment env,[FromServices] DAL<Artista> dal, [FromBody] ArtistaRequest artistaRequest) =>
+        {
+            
+            var nome = artistaRequest.nome.Trim();
+            var imagemArtista = DateTime.Now.ToString("ddMMyyyyhhss") + "." + nome + ".jpg";
+
+            var path = Path.Combine(env.ContentRootPath,
+                "wwwroot", "FotosPerfil", imagemArtista);
+
+            using MemoryStream ms = new MemoryStream(Convert.FromBase64String(artistaRequest.fotoPerfil!));
+            using FileStream fs = new(path, FileMode.Create);
+            await ms.CopyToAsync(fs);
+
+            var artista = new Artista(artistaRequest.nome, artistaRequest.bio) { FotoPerfil = $"/FotosPerfil/{imagemArtista}" };
+
+            dal.Adicionar(artista);
+            return Results.Ok();
+        });
+
+        group.MapDelete("{id}", ([FromServices] DAL<Artista> dal, int id) => {
+            var artista = dal.RecuperarPor(a => a.Id == id);
+            if (artista is null)
+            {
+                return Results.NotFound();
+            }
+            dal.Deletar(artista);
+            return Results.NoContent();
+
+        });
+
+        group.MapPut("", ([FromServices] DAL<Artista> dal, [FromBody] ArtistaRequestEdit artistaRequestEdit) => {
+            var artistaAAtualizar = dal.RecuperarPor(a => a.Id == artistaRequestEdit.Id);
+            if (artistaAAtualizar is null)
+            {
+                return Results.NotFound();
+            }
+            artistaAAtualizar.Nome = artistaRequestEdit.nome;
+            artistaAAtualizar.Bio = artistaRequestEdit.bio;        
+            dal.Atualizar(artistaAAtualizar);
+            return Results.Ok();
+        });
+        #endregion
+    }
+
+    private static ICollection<ArtistaResponse> EntityListToResponseList(IEnumerable<Artista> listaDeArtistas)
+    {
+        return listaDeArtistas.Select(a => EntityToResponse(a)).ToList();
+    }
+
+    private static ArtistaResponse EntityToResponse(Artista artista)
+    {
+        return new ArtistaResponse(artista.Id, artista.Nome, artista.Bio, artista.FotoPerfil);
+    }
+
+  
+}
